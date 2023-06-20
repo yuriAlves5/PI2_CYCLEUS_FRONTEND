47ea765ff5ef1a7f850c0e8e69c4af44a7d99876
Guilherme Leal <guilhermelml@gmail.com>
2023-06-20 01:11:16 -0300

    Chore: alteração app.js home e styles (idvisual)

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export const theme = createTheme({
+    colors: {
+        primary: '#011140',
+        secondary: '#ffffff',
+        accent: '#021e72',
+        background: '#00040e',
+        lightGray: '#e6e6e6',
+        darkBlue: '#032ca4',
+        error: '#FF0000'
+    },
+});

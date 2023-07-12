dc5067fbf108f195214f5fa4c38d63974a2784de
Yuri Alves <yurivalerio12@gmail.com>
2023-07-12 02:56:24 -0300

    route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import Station from './pages/Station';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Station />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/station" element={<Station />} />
       </Routes>
     </Router>
   );
